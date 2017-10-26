3f48e64b892fe47608e22fa4cace5ae166c62029
cheezu <cheesevoltaire@gmail.com>
2017-10-26 20:49:12 +0800

    Created LoginPage

--- LoginPage.aspx.cs
+++ LoginPage.aspx.cs
@@ -4,11 +4,41 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
 
 public partial class LoginPage : System.Web.UI.Page
 {
-    protected void Page_Load(object sender, EventArgs e)
+    protected void LoginButton_Click(object sender, EventArgs e)
     {
 
+        SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["secretarydb"].ConnectionString);
+
+        try
+        {
+            con.Open();
+            SqlCommand cmd = new SqlCommand("SELECT * FROM SecretaryUser WHERE UserName=@username and Password=@password", con);
+            cmd.Parameters.AddWithValue("@username", txtUser.Text);
+            cmd.Parameters.AddWithValue("@password", txtPWD.Text);
+
+            SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            adapter.Fill(dt);
+
+            if (dt.Rows.Count > 0)
+            {
+                Response.Redirect("MainPage.aspx");
+            }
+            else
+            {
+                ClientScript.RegisterStartupScript(Page.GetType(), "validation", "<script language='javascript'>alert('Invalid Username and Password')</script>");
+            }
+        }
+
+        finally
+        {
+            con.Close();
+        }
     }
 }
