15e748be31511d337194d199cb026eaf23e9eee2
xmayanksehgal <mayank@mayanksehgal.com>
2017-10-26 18:18:34 +0530

    Department Affairs Welcome Page
    Dept Affairs welcome page is done

--- Department Affairs/DepartmentWelcomePage.aspx.cs
+++ Department Affairs/DepartmentWelcomePage.aspx.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -9,6 +10,21 @@ public partial class DepartmentWelcomePage : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
+        if (!IsPostBack)
+        {
+            this.GetData();
+        }
+    }
 
+    private void GetData()
+    {
+        DataView dv_money = (DataView)moneyleft_sqlsource.Select(DataSourceSelectArguments.Empty);
+        DataRowView drv = dv_money[0];
+
+        DataView dv_research = (DataView)researchnumber_sqlsource.Select(DataSourceSelectArguments.Empty);
+        DataRowView drv2 = dv_research[0];
+
+        moneyleft_data.Text = drv["SumAmount"].ToString();
+        researchnumber_data.Text = drv2["ResearchCount"].ToString();
     }
 }
